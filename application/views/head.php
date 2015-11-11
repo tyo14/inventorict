b7885a668a2b3c0f18a523ddbfd9f34881bdc561
gunawan820 <gupuspo@gmail.com>
2015-11-11 23:19:23 +0700

    Ubah icon dashboard dan master

--- application/views/head.php
+++ application/views/head.php
@@ -312,10 +312,10 @@
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             <li class="header">Navigasi Utama</li>
-            <li class="active"><a href="documentation/index.html"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
+            <li class="active"><a href="documentation/index.html"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Master</span> <i class="fa fa-angle-left pull-right"></i>
+                <i class="fa fa-book"></i> <span>Master</span> <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
                 <li><a href="index.html"><i class="fa fa-circle-o"></i> Daftar Barang</a></li>

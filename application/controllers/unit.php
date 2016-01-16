781ec0a5d8f03a8ccbc9b94a1de253c8296ee518
gunawan820 <gupuspo@gmail.com>
2016-01-16 15:51:30 +0700

    ubah unit fix

--- application/controllers/unit.php
+++ application/controllers/unit.php
@@ -78,38 +78,6 @@
 
  	public function ubah($id)
  	{
- 		/*(if($this->input->post('saveunit')){
-
- 			$data = $this->input->post();
- 			$data['kode_unit'] = strtoupper($data['kode_unit']);
- 			$get = $data['kode_unit'];
- 			unset($data['saveunit']);
-
- 			$this->global_model->update('unit',$data, array('kode_unit' => $id));
-
- 			if($data['kode_unit'] != $id){
- 				$url = 'unit/ubah/'.$data['kode_unit'];
-
- 				foreach ($this->global_model->search('barang',array('kode_barang' => $id),null,null,null,0) as $row) {
- 					list($kodes,$digits) = explode('-', $row['kode_barang']);
- 					$ubah = array(
- 						'kode_barang' => $get.'-'.$digits,
- 						'tgl_beli' => $row['tgl_beli'],
- 						'nama_barang' => $row['nama_barang'],
- 						'deskripsi' => $row['deskripsi'],
- 						'kode_unit' => $row['kode_unit']);
-
- 					$this->global_model->update('barang',$ubah,array('kode_unit' => $row['kode_unit']));
- 				}
-
- 			}else{
- 				$url = 'unit/ubah/'.$id;
- 			}
-
- 			redirect(site_url($url));
-
- 		}*/
-
  		$data['unit'] = $this->global_model->find_by('unit', array('kode_unit' => $id));
  		$data['kategori'] = $this->global_model->find_all('kategori');
  		$this->load->view('head');
